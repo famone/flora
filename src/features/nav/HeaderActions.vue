4954f0b226146b7195c1d9a6569404cf48f00cb6
Konstantin Timofeev <mrfamone@mail.ru>
2023-11-21 00:46:23 +0300

    fix header actions

--- src/features/nav/HeaderActions.vue
+++ src/features/nav/HeaderActions.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="flex gap-2">
-        <router-link v-for="(item, idx) in actions" :key="idx" :to="item.link" @click="item.action">
-            <div class="cursor-pointer text-center p-2 text-xs hover:translate-y-1 transition-all relative">
-                <span v-if="item.bage && cartStore.cart.length" class="bage">{{ cartStore.cart.length }}</span>
-                <component :is="item.component" class="h-5 mx-auto mb-1" />
-                {{ item.txt }}
-            </div>
-        </router-link>
+        <div class="cursor-pointer text-center p-2 text-xs hover:translate-y-1 transition-all relative"
+            v-for="(item, idx) in actions" :key="idx" @click.prevent="item.action">
+            <span v-if="item.bage && cartStore.cart.length" class="bage">{{ cartStore.cart.length }}</span>
+            <component :is="item.component" class="h-5 mx-auto mb-1" />
+            {{ item.txt }}
+        </div>
         <transition name="fade-slide">
             <LoginModal v-if="showLoginForm" @close-modal="showLoginForm = !showLoginForm" />
         </transition>
@@ -18,6 +17,7 @@ import { ref } from 'vue'
 import LoginModal from '@/features/nav/modals/LoginModal.vue'
 import { UserIcon, ShoppingCartIcon, HeartIcon } from '@heroicons/vue/24/outline'
 import { useCartStore } from '@/stores/cart'
+import router from '@/router/index'
 
 const cartStore = useCartStore()
 const showLoginForm = ref(false)
@@ -29,18 +29,21 @@ const actions = ref([
         action: () => {
             showLoginForm.value = true
         },
-        link: '/'
     },
     {
         txt: 'Избранное',
         component: HeartIcon,
-        link: '/'
+        action: () => {
+            router.push('/favourites')
+        },
     },
     {
         txt: 'Корзина',
         component: ShoppingCartIcon,
         bage: true,
-        link: '/cart'
+        action: () => {
+            router.push('/cart')
+        },
     }
 ])
 </script>
